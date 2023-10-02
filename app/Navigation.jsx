4388b6182aed6f296f6819e9085b2562f73cdb2a
Michael Jeung <michaelj229@gmail.com>
2023-10-01 20:12:11 -0400

    inital commit

---
+++ app/Navigation.jsx
@@ -0,0 +1,17 @@
+export default function Navigation() {
+    return (
+        <nav>
+            <div>
+                <a href="#methods">BREW</a>
+            </div>
+            <div>
+                <a href="#home">
+                    <img src="coffee-icon.png" alt="icon" />
+                </a>
+            </div>
+            <div>
+                <a href="Contact">CONTACT</a>
+            </div>
+        </nav>
+    )
+}
