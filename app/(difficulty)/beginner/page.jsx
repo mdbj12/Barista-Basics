c833f99861db3c0ca46ee405b6adb424249567e5
Michael Jeung <michaelj229@gmail.com>
2023-10-21 14:10:42 -0400

    labeled function name for beginner

--- app/(difficulty)/beginner/page.jsx
+++ app/(difficulty)/beginner/page.jsx
@@ -1,6 +1,6 @@
 import Link from "next/link";
 
-export default function () {
+export default function Beginner() {
     return (
         <div className="beginner-intro">
             <div className="beginner-title">
